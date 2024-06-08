507a8b251e38af299320be4fe3f614dd0ab90b03
uwu-owo-o3o <lance_d_ong@dlsu.edu.ph>
2024-06-08 11:53:13 +0800

    drag and drop script works

---
+++ Assets/Scripts/DragAndDrop/DragAndDrop.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    Vector3 mousePosition;    
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private Vector3 GetMousePos()
+    {
+        return Camera.main.WorldToScreenPoint(this.transform.position);
+    }
+
+    private void OnMouseDown()
+    {
+        this.mousePosition = Input.mousePosition - GetMousePos();
+    }
+
+    private void OnMouseDrag()
+    {
+        this.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
+    }
+}
