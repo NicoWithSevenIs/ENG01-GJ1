ab61772a96e1a54bffa9a1ada52ca2e3247c4137
uwu-owo-o3o <lance_d_ong@dlsu.edu.ph>
2024-06-14 16:27:50 +0800

    rotate cam kinda works

---
+++ Assets/Scripts/Effects/TriggerCameraPan.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCameraPan : MonoBehaviour
+{
+
+    [SerializeField] private Transform target;
+    [SerializeField] private TransformBottle bottleScript;
+
+    private float rotateSpeed = 20.0f;
+    private float rotateThreshold = 3.3f;
+    private bool willLookAtTable = true;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        Vector3 pos = this.transform.eulerAngles;
+
+        if (this.bottleScript.triggerCam && pos.x >= -60.0f)
+        {
+            pos.x -= Time.deltaTime * rotateSpeed;
+        }
+        else if (!this.bottleScript.triggerCam && pos.x <= 0.0f)
+        {
+            pos.x += Time.deltaTime * rotateSpeed;
+                
+        }
+
+        transform.eulerAngles = pos;
+        
+        
+    }
+}
